b585a9f5d7b6028cfac37c3b84e54120ec3c7e02
denisshashkov <shashkov-56@mail.ru>
2021-11-22 15:57:25 +0300

    add animation for HeaderCartButton

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -27,6 +27,12 @@ const DUMMY_MEALS = [
     description: "Healthy...and green...",
     price: 18.99,
   },
+  {
+    id: "m5",
+    name: "Pizza",
+    description: "Yummy! Yummy!",
+    price: 24.32,
+  },
 ];
 
 const AvailableMeals = () => {
